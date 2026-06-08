fc68847c07b86dbf736ad627181e85bcec81c70a
febriinanda <febriinanda@gmail.com>
2026-06-08 21:48:36 +0700

    feat: update employee's gender
    Signed-off-by: febriinanda <febriinanda@gmail.com>

--- src/main/java/com/lite/hris/person/PersonController.java
+++ src/main/java/com/lite/hris/person/PersonController.java
@@ -61,6 +61,16 @@ public class PersonController {
         }else throw new RuntimeException("Person is not found");
     }
 
+    @PatchMapping("/{id}/gender")
+    public void gender(@PathVariable long id, @RequestBody PersonDTO form){
+        Optional<Person> byId = repository.findById(id);
+        if(byId.isPresent()){
+            Person existed = byId.get();
+            existed.setGender(form.getGender());
+            repository.save(existed);
+        }else throw new RuntimeException("Person is not found");
+    }
+
     @DeleteMapping("/{id}")
     public void delete(@PathVariable long id){
         Optional<Person> byId = repository.findById(id);
