9c775195669045704f3640518fe7c2f1d2d0b5e4
febriinanda <febriinanda@gmail.com>
2026-06-10 22:31:30 +0700

    feat: remove document category module because enum is better
    Signed-off-by: febriinanda <febriinanda@gmail.com>

--- src/main/java/com/lite/hris/document/category/DocumentCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lite.hris.document.category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DocumentCategoryRepository extends JpaRepository<DocumentCategory, Long> {
-}
