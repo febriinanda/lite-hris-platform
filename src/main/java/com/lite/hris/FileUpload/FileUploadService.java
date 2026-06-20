f6918fcedc761f1a518fd0831631f48d6a27b9b8
febriinanda <febriinanda@gmail.com>
2026-06-20 07:52:30 +0700

    feat: method to handle photo profile upload
    Signed-off-by: febriinanda <febriinanda@gmail.com>

--- src/main/java/com/lite/hris/FileUpload/FileUploadService.java
+++ src/main/java/com/lite/hris/FileUpload/FileUploadService.java
@@ -1,6 +1,5 @@
 package com.lite.hris.FileUpload;
 
-import com.lite.hris.document.employment.EmploymentDocument;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,6 +29,23 @@ public class FileUploadService {
             doc.setFilePath(path.toString());
             doc.setFileSize(file.getSize());
             doc.setUploadDate(LocalDateTime.now());
+        }
+    }
+
+    public void photoProfile(HasFileUpload p, MultipartFile file) throws IOException {
+        if(file != null && !file.isEmpty()){
+            String ext = StringUtils.getFilenameExtension(file.getOriginalFilename());
+            String fileName = UUID.randomUUID().toString();
+            String newFileName = fileName+"."+ext;
+            Path documentStorage = Paths.get("storage/photo-profile");
+            Files.createDirectories(documentStorage);
+            Path path = documentStorage.resolve(newFileName);
+            Files.copy(file.getInputStream(), path, REPLACE_EXISTING);
+
+            p.setFileName(newFileName);
+            p.setFilePath(path.toString());
+            p.setFileSize(file.getSize());
+            p.setUploadDate(LocalDateTime.now());
         }
     }
 }
