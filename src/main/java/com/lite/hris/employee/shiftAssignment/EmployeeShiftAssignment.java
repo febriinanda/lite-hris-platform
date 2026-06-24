723e703a26f8621625844f1d62690acd111568b3
febriinanda <febriinanda@gmail.com>
2026-06-24 20:30:09 +0700

    feat: prepare employee shift assignment
    Signed-off-by: febriinanda <febriinanda@gmail.com>

---
+++ src/main/java/com/lite/hris/employee/shiftAssignment/EmployeeShiftAssignment.java
@@ -0,0 +1,29 @@
+package com.lite.hris.employee.shiftAssignment;
+
+import com.lite.hris.employee.Employee;
+import com.lite.hris.shift.pattern.ShiftPattern;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "employee_shift_assignment")
+@NoArgsConstructor
+public class EmployeeShiftAssignment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "shift_pattern_id")
+    private ShiftPattern pattern;
+
+    private LocalDate effectiveDate;
+}
