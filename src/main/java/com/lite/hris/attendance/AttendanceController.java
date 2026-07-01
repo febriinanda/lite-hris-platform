c0af30c3d3e3f1adbbec34c1418f8811c472fe19
febriinanda <febriinanda@gmail.com>
2026-07-01 20:31:58 +0700

    refactor: apply attendance log to employee attendance based on employee schedule
    Signed-off-by: febriinanda <febriinanda@gmail.com>

--- src/main/java/com/lite/hris/attendance/AttendanceController.java
+++ src/main/java/com/lite/hris/attendance/AttendanceController.java
@@ -1,5 +1,6 @@
 package com.lite.hris.attendance;
 
+import com.lite.hris.employee.Employee;
 import com.lite.hris.employee.attendance.EmployeeAttendance;
 import com.lite.hris.employee.attendance.EmployeeAttendanceRepository;
 import com.lite.hris.employee.attendance.VerificationStatus;
@@ -8,10 +9,8 @@ import com.lite.hris.employee.schedule.EmployeeScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -28,13 +27,29 @@ public class AttendanceController {
         Map<Long, EmployeeAttendance> attendanceMap = employeeAttendanceRepository.findByScheduleIn(byScheduleDate).stream()
                 .collect(Collectors.toMap(o -> o.getSchedule().getId(), Function.identity()));
 
+        LocalDateTime min = LocalDateTime.MAX;
+        LocalDateTime max = LocalDateTime.MIN;
+
+        for (EmployeeSchedule s : byScheduleDate) {
+            if(s.isOff())
+                continue;
+
+            min = s.getStartDate().isBefore(min)?s.getStartDate():min;
+            max = s.getEndDate().isAfter(max)?s.getEndDate():max;
+        }
+
+        Map<Employee, List<AttendanceLog>> attendanceLogsPerEmployee = attendanceLogRepository.findByTimeBetween(min.minusHours(2), max.plusHours(4))
+                .stream().collect(Collectors.groupingBy(AttendanceLog::getEmployee));
+
         List<EmployeeAttendance> changes = new ArrayList<>();
         for (EmployeeSchedule s : byScheduleDate) {
+            List<AttendanceLog> logs = attendanceLogsPerEmployee.getOrDefault(s.getEmployee(), new ArrayList<>());
             EmployeeAttendance a = attendanceMap.get(s.getId());
             if(a == null){
                 a = new EmployeeAttendance(s, VerificationStatus.PENDING);
             }
 
+            a.updateClock(logs);
             a.check();
             changes.add(a);
         }
