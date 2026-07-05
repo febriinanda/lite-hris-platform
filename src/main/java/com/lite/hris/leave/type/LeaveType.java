c47caba513705dc6d62cfa6c26f1a2ef8f2c1f39
febriinanda <febriinanda@gmail.com>
2026-07-05 21:01:51 +0700

    wip: LeaveType.java to control leave request
    Signed-off-by: febriinanda <febriinanda@gmail.com>

---
+++ src/main/java/com/lite/hris/leave/type/LeaveType.java
@@ -0,0 +1,20 @@
+package com.lite.hris.leave.type;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "leave_type")
+@Data
+@NoArgsConstructor
+public class LeaveType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String code;
+    private boolean needAttachment;
+    private boolean consumeBalance;
+    private int maxDays;
+}
