f7416178015105ede45f3608a32b2fcaa7382fdb
febriinanda <febriinanda@gmail.com>
2026-07-18 21:51:11 +0700

    refactor: provide attendance verification service
    Signed-off-by: febriinanda <febriinanda@gmail.com>

--- src/main/java/com/lite/hris/attendance/AttendanceController.java
+++ src/main/java/com/lite/hris/attendance/AttendanceController.java
@@ -1,13 +1,9 @@
 package com.lite.hris.attendance;
 
 import com.lite.hris.employee.Employee;
-import com.lite.hris.employee.attendance.*;
-import com.lite.hris.employee.leave.grant.EmployeeLeaveGrant;
-import com.lite.hris.employee.leave.grant.EmployeeLeaveGrantRepository;
-import com.lite.hris.employee.leave.transaction.EmployeeLeaveTransaction;
-import com.lite.hris.employee.leave.transaction.EmployeeLeaveTransactionRepository;
-import com.lite.hris.employee.leave.transaction.LeaveReferenceType;
-import com.lite.hris.employee.leave.transaction.LeaveTransactionType;
+import com.lite.hris.employee.attendance.EmployeeAttendance;
+import com.lite.hris.employee.attendance.EmployeeAttendanceRepository;
+import com.lite.hris.employee.attendance.VerificationStatus;
 import com.lite.hris.employee.schedule.EmployeeSchedule;
 import com.lite.hris.employee.schedule.EmployeeScheduleRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +11,9 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -26,8 +24,9 @@ public class AttendanceController {
     private final EmployeeScheduleRepository employeeScheduleRepository;
     private final EmployeeAttendanceRepository employeeAttendanceRepository;
     private final AttendanceLogRepository attendanceLogRepository;
-    private final EmployeeLeaveGrantRepository employeeLeaveGrantRepository;
-    private final EmployeeLeaveTransactionRepository employeeLeaveTransactionRepository;
+    private final AttendanceVerificationService attendanceVerificationService;
+
+
     private final ApplicationEventPublisher publisher;
     @PostMapping("/process")
     public void process(@RequestBody AttendanceProcessRequest form){
@@ -67,45 +66,7 @@ public class AttendanceController {
 
     @PatchMapping("/{id}/verify")
     public void verify(@PathVariable long id, @RequestBody AttendanceVerificationRequest form){
-        Optional<EmployeeAttendance> byId = employeeAttendanceRepository.findById(id);
-        if(byId.isPresent()){
-            EmployeeAttendance a = byId.get();
-            a.verified(form);
-
-            if(a.getVerificationStatus() == VerificationStatus.VERIFIED || a.getVerificationStatus() == VerificationStatus.AUTO_VERIFIED){
-                if(a.getAction() == AttendanceFollowUp.LEAVE_DEDUCTION){
-                    Optional<EmployeeLeaveGrant> min = employeeLeaveGrantRepository.findByEmployee(a.getSchedule().getEmployee()).stream().filter(
-                            o -> o.getEarnedDate().isBefore(a.getAttendanceDate()) && o.getFinalExpireDate().isAfter(a.getAttendanceDate())
-                    ).min(Comparator.comparing(EmployeeLeaveGrant::getFinalExpireDate));
-
-                    EmployeeLeaveTransaction t = new EmployeeLeaveTransaction();
-                    t.setCreatedAt(LocalDateTime.now());
-                    t.setReferenceType(LeaveReferenceType.ATTENDANCE);
-                    t.setReferenceId(a.getId());
-                    t.setEmployee(a.getSchedule().getEmployee());
-                    t.setAmount(-1);
-                    LeaveTransactionType type = null;
-                    if(a.getState() == AttendanceState.ABSENT || a.getState() == AttendanceState.INCOMPLETE)
-                        type = LeaveTransactionType.ABSENCE_CONVERSION;
-                    else if(a.getDayType() == DayType.LEAVE)
-                        type = LeaveTransactionType.LEAVE_APPROVED;
-                    t.setTransactionType(type);
-                    t.setCreatedBy(form.getVerifiedBy().getPerson().getName());
-
-                    if(min.isPresent()){
-                        EmployeeLeaveGrant grant = min.get();
-                        t.setGrant(grant);
-                        grant.setRemainingDays(grant.getRemainingDays() + t.getAmount());
-                        employeeLeaveGrantRepository.save(grant);
-                    }
-
-                    employeeLeaveTransactionRepository.save(t);
-                }
-            }
-            employeeAttendanceRepository.save(a);
-
-
-        }else throw new RuntimeException("This attendance is not exist");
+        attendanceVerificationService.verify(id, form);
     }
 
     @PostMapping("/clock")
