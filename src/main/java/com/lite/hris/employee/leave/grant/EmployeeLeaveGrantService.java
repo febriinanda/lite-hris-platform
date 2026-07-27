403f2c4471535ce3570bba9e1e860369d636fc14
febriinanda <febriinanda@gmail.com>
2026-07-27 21:05:39 +0700

    refactor: provide employee leave grant service
    Signed-off-by: febriinanda <febriinanda@gmail.com>

---
+++ src/main/java/com/lite/hris/employee/leave/grant/EmployeeLeaveGrantService.java
@@ -0,0 +1,41 @@
+package com.lite.hris.employee.leave.grant;
+
+import com.lite.hris.employee.Employee;
+import com.lite.hris.employee.EmployeeRepository;
+import com.lite.hris.employee.leave.transaction.EmployeeLeaveTransaction;
+import com.lite.hris.employee.leave.transaction.EmployeeLeaveTransactionRepository;
+import com.lite.hris.employee.leave.transaction.LeaveReferenceType;
+import com.lite.hris.employee.leave.transaction.LeaveTransactionType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeLeaveGrantService {
+    private final EmployeeLeaveGrantRepository employeeLeaveGrantRepository;
+    private final EmployeeLeaveTransactionRepository employeeLeaveTransactionRepository;
+    private final EmployeeRepository employeeRepository;
+    public void grant(long id, LeaveBalanceGrantRequest form){
+        Optional<Employee> byId = employeeRepository.findById(id);
+        if(byId.isPresent()){
+            List<EmployeeLeaveGrant> existed = employeeLeaveGrantRepository.findByEmployeeAndYear(byId.get(), form.getYear());
+            if(!existed.isEmpty())
+                throw new RuntimeException("This employee has beed granted this year");
+
+            EmployeeLeaveGrant grant = new EmployeeLeaveGrant(form);
+            EmployeeLeaveTransaction t = new EmployeeLeaveTransaction();
+            t.setEmployee(form.getEmployee());
+            t.setGrant(grant);
+            t.setAmount(form.getGrantedDays());
+            t.setTransactionType(LeaveTransactionType.ENTITLEMENT);
+            t.setReferenceType(LeaveReferenceType.SYSTEM);
+            t.setCreatedAt(LocalDateTime.now());
+            employeeLeaveGrantRepository.save(grant);
+            employeeLeaveTransactionRepository.save(t);
+        }else throw new RuntimeException("This employee is not found");
+    }
+}
