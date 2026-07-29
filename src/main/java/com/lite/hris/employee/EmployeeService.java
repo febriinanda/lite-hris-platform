523b0ed3ac01dfaaa56ef0d7bb6bb58be28f8440
febriinanda <febriinanda@gmail.com>
2026-07-29 21:33:20 +0700

    refactor: provide employee service
    Signed-off-by: febriinanda <febriinanda@gmail.com>

---
+++ src/main/java/com/lite/hris/employee/EmployeeService.java
@@ -0,0 +1,26 @@
+package com.lite.hris.employee;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+    private final EmployeeRepository repository;
+
+    public List<Employee> findAll(){
+        return repository.findAll();
+    }
+
+    public Employee findById(long id){
+        Optional<Employee> byId = repository.findById(id);
+        if(byId.isPresent()){
+            return byId.get();
+        }else throw new RuntimeException("Employee is not found");
+    }
+
+
+}
