3402beee84e9408ad290c56e82f031c6ab5c566d
febriinanda <febriinanda@gmail.com>
2026-08-04 20:56:38 +0700

    refactor: provide person service
    Signed-off-by: febriinanda <febriinanda@gmail.com>

--- src/main/java/com/lite/hris/person/PersonController.java
+++ src/main/java/com/lite/hris/person/PersonController.java
@@ -1,97 +1,61 @@
 package com.lite.hris.person;
 
-import com.lite.hris.FileUpload.FileUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/person")
 @RequiredArgsConstructor
 public class PersonController {
-    private final PersonRepository repository;
-    private final FileUploadService fileUploadService;
+    private final PersonService service;
+    private final PhotoProfileUploaderService photoProfileUploader;
 
     @GetMapping
     public List<Person> findAll(){
-        return repository.findAll();
+        return service.findAll();
     }
 
     @GetMapping("/{id}")
     public Person findById(@PathVariable long id){
-        Optional<Person> byId = repository.findById(id);
-        if(byId.isPresent()){
-            return byId.get();
-        }else throw new RuntimeException("Person is not found");
+        return service.findById(id);
     }
 
     @PostMapping
     public void create(@RequestBody PersonDTO form){
-        Person p = new Person(form);
-        repository.save(p);
+        service.create(form);
     }
 
     @PutMapping("/{id}")
     public void update(@PathVariable long id, @RequestBody PersonDTO form){
-        Optional<Person> byId = repository.findById(id);
-        if(byId.isPresent()){
-            Person existed = byId.get();
-            existed.update(form);
-            repository.save(existed);
-        }else throw new RuntimeException("Person is not found");
+        service.update(id, form);
     }
 
     @PatchMapping("/{id}/name")
     public void rename(@PathVariable long id, @RequestBody PersonDTO form){
-        Optional<Person> byId = repository.findById(id);
-        if(byId.isPresent()){
-            Person existed = byId.get();
-            existed.setName(form.getName());
-            repository.save(existed);
-        }else throw new RuntimeException("Person is not found");
+        service.rename(id, form);
     }
 
     @PatchMapping("/{id}/birthday")
     public void birthday(@PathVariable long id, @RequestBody PersonDTO form){
-        Optional<Person> byId = repository.findById(id);
-        if(byId.isPresent()){
-            Person existed = byId.get();
-            existed.setBirthDate(form.getBirthDate());
-            repository.save(existed);
-        }else throw new RuntimeException("Person is not found");
+        service.birthDayUpdate(id, form);
     }
 
     @PatchMapping("/{id}/gender")
     public void gender(@PathVariable long id, @RequestBody PersonDTO form){
-        Optional<Person> byId = repository.findById(id);
-        if(byId.isPresent()){
-            Person existed = byId.get();
-            existed.setGender(form.getGender());
-            repository.save(existed);
-        }else throw new RuntimeException("Person is not found");
+        service.genderSwitch(id, form);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable long id){
-        Optional<Person> byId = repository.findById(id);
-        if(byId.isPresent()){
-            Person existed = byId.get();
-            existed.setDeleted(true);
-            repository.save(existed);
-        }else throw new RuntimeException("Person is not found");
+        service.delete(id);
     }
 
     @PatchMapping("/{id}/photo/profile")
     public void uploadPhotoProfile(@PathVariable long id, @RequestPart("file")MultipartFile file) throws IOException {
-        Optional<Person> byId = repository.findById(id);
-        if(byId.isPresent()){
-            Person person = byId.get();
-            fileUploadService.photoProfile(person, file);
-            repository.save(person);
-        }
+        photoProfileUploader.upload(id, file);
     }
 }
---
+++ src/main/java/com/lite/hris/person/PersonService.java
@@ -0,0 +1,55 @@
+package com.lite.hris.person;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PersonService {
+    private final PersonRepository repository;
+
+    public List<Person> findAll(){
+        return repository.findAll();
+    }
+
+    public Person findById(long id){
+        Optional<Person> byId = repository.findById(id);
+        if(byId.isPresent()){
+            return byId.get();
+        }else throw new RuntimeException("Person is not found");
+    }
+
+
+    public void create(PersonDTO form) {
+        Person p = new Person(form);
+        repository.save(p);
+    }
+
+    public void update(long id, PersonDTO form) {
+        Person p = findById(id);
+        p.update(form);
+    }
+
+    public void rename(long id, PersonDTO form) {
+        Person p = findById(id);
+        p.setName(form.getName());
+    }
+
+    public void birthDayUpdate(long id, PersonDTO form) {
+        Person p = findById(id);
+        p.setBirthDate(form.getBirthDate());
+    }
+
+    public void genderSwitch(long id, PersonDTO form) {
+        Person p = findById(id);
+        p.setGender(form.getGender());
+    }
+
+    public void delete(long id) {
+        Person p = findById(id);
+        p.delete();
+    }
+}
