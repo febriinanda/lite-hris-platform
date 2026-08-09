e3b4008e44ad98d6b3444d31d0e2d47e57a81ed9
febriinanda <febriinanda@gmail.com>
2026-08-09 12:54:35 +0700

    refactor: provide approval service
    Signed-off-by: febriinanda <febriinanda@gmail.com>

--- src/main/java/com/lite/hris/approval/ApprovalController.java
+++ src/main/java/com/lite/hris/approval/ApprovalController.java
@@ -1,107 +1,18 @@
 package com.lite.hris.approval;
 
-import com.lite.hris.approval.task.ApprovalStatus;
-import com.lite.hris.approval.task.ApprovalTask;
-import com.lite.hris.approval.task.ApprovalTaskRepository;
-import com.lite.hris.fact.leave.LeaveFact;
-import com.lite.hris.fact.leave.LeaveFactRepository;
-import com.lite.hris.request.RequestStatus;
-import com.lite.hris.request.RequestType;
-import com.lite.hris.request.leave.LeaveRequest;
-import com.lite.hris.request.leave.LeaveRequestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.*;
-import java.util.stream.Collectors;
-
 @RestController
 @RequestMapping("/approval")
 @RequiredArgsConstructor
 public class ApprovalController {
-    private final ApprovalTaskRepository approvalTaskRepository;
-    private final LeaveRequestRepository leaveRequestRepository;
-    private final LeaveFactRepository leaveFactRepository;
+    private final ApprovalService service;
     @PostMapping
     public void submit(@RequestBody ApprovalForm form){
-        Optional<ApprovalTask> byId = approvalTaskRepository.findById(form.getApprovalTaskId());
-        if(byId.isEmpty())
-            throw new RuntimeException("This approval task is not found");
-
-        ApprovalTask task = byId.get();
-        List<ApprovalTask> approvals = approvalTaskRepository.findByRequestTypeAndRequestId(task.getRequestType(), task.getRequestId());
-        Map<Integer, List<ApprovalTask>> approvalMaps = approvals.stream().collect(Collectors.groupingBy(ApprovalTask::getSequence));
-        List<ApprovalTask> prevWaves = approvalMaps.getOrDefault(task.getSequence() - 1, new ArrayList<>()).stream()
-                .filter(o -> o.getStatus().equals(ApprovalStatus.APPROVED))
-                .toList();
-
-        int minimum = prevWaves.stream()
-                .map(ApprovalTask::getMinimumApprovalThisSequence)
-                .min(Comparator.comparing(o -> o)).orElse(0);
-
-        if(prevWaves.size() < minimum){
-            throw new RuntimeException("Last wave of approval is not approved properly");
-        }
-
-        if(form.getStatus().equals(ApprovalStatus.REJECTED)){
-            task.setStatus(form.getStatus());
-            approvalTaskRepository.save(task);
-
-            if(task.getRequestType().equals(RequestType.LEAVE_TYPE)){
-                Optional<LeaveRequest> requestOptional = leaveRequestRepository.findById(task.getRequestId());
-                if(requestOptional.isPresent()){
-                    LeaveRequest request = requestOptional.get();
-                    request.setStatus(RequestStatus.REJECTED);
-                    leaveRequestRepository.save(request);
-                }
-            }
-        }
-
-        if(form.getStatus().equals(ApprovalStatus.APPROVED)){
-            task.setStatus(form.getStatus());
-            approvalTaskRepository.save(task);
-
-            List<ApprovalTask> currentWaves = approvalMaps.getOrDefault(task.getSequence(), new ArrayList<>())
-                    .stream()
-                    .filter(o -> o.getStatus().equals(ApprovalStatus.APPROVED))
-                    .toList();
-
-            minimum = currentWaves.stream().map(ApprovalTask::getMinimumApprovalThisSequence)
-                    .min(Comparator.comparing(o -> o))
-                    .orElse(0);
-
-            if(currentWaves.size()>=minimum){
-                List<ApprovalTask> nextWaves = approvalMaps.getOrDefault(task.getSequence() + 1, new ArrayList<>());
-                if(nextWaves.isEmpty()){
-                    if(task.getRequestType().equals(RequestType.LEAVE_TYPE)){
-                        Optional<LeaveRequest> requestOptional = leaveRequestRepository.findById(task.getRequestId());
-                        if(requestOptional.isPresent()){
-                            LeaveRequest request = requestOptional.get();
-                            request.setStatus(RequestStatus.APPROVED);
-                            leaveRequestRepository.save(request);
-
-                            LocalDate start = request.getStartDate();
-                            List<LeaveFact> facts = new ArrayList<>();
-                            while(!start.isAfter(request.getEndDate())){
-                                LeaveFact f = new LeaveFact();
-                                f.setCode(request.getType().getCode());
-                                f.setReference(request);
-                                f.setEmployee(request.getEmployee());
-                                f.setAttendanceDate(start);
-                                f.setConsumeBalance(request.getType().isConsumeBalance());
-                                facts.add(f);
-                                start = start.plusDays(1);
-                            }
-
-                            leaveFactRepository.saveAll(facts);
-                        }
-                    }
-                }
-            }
-        }
+        service.submit(form);
     }
 }
